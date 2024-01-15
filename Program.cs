072a5cb9037356833a08cd439606390156dc2e7b
Roman Mukhortov <romanm163@yandex.ru>
2024-01-15 22:01:08 +0400

    на проверку

--- Program.cs
+++ Program.cs
@@ -1,161 +1,87 @@
-﻿
-
-
-/* Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает 
- значение этого элемента или же указание, что такого элемента нет. */
-
-// Console.Clear();
-// Console.Write("Введите строку: ");
-// int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
-// Console.Write("Введите столбец: ");
-// int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
-// int n = 5; 
-// int m = 7; 
-// Random random = new Random();
-// int[,] arr = new int[n, m];
-// Console.WriteLine("Массив: ");
-// for (int i = 0; i < arr.GetLength(0); i++)
-// {
-//     for (int j = 0; j < arr.GetLength(1); j++)
-// {
-//     arr[i, j] = random.Next(10, 99);
-// Console.Write("{0} ", arr[i, j]);
+﻿//  // Задача 1. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+ 
+// Console.Write("Введите число M: "); 
+// int m = Convert.ToInt32(Console.ReadLine()); 
+ 
+// Console.Write("Введите число N: "); 
+// int n = Convert.ToInt32(Console.ReadLine()); 
+ 
+// SumFromMToN(m, n); 
+ 
+// void SumFromMToN(int m, int n) 
+// { 
+//  Console.Write(SumMN(m - 1, n)); 
+// } 
+ 
+// int SumMN(int m, int n) 
+// { 
+//  int res = m; 
+//  if (m == n) 
+//  return 0; 
+//  else 
+//  { 
+//  m++; 
+//  res = m + SumMN(m, n); 
+//  return res; 
+//  } 
 // }
-// Console.WriteLine();
+
+
+
+// =============================================================================
+// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+ 
+// Console.Write("Введите число M: "); 
+// int m = Convert.ToInt32(Console.ReadLine()); 
+ 
+// Console.Write("Введите число N: "); 
+// int n = Convert.ToInt32(Console.ReadLine()); 
+ 
+// AkkermanFunction(m, n); 
+ 
+// void AkkermanFunction(int m, int n) 
+// { 
+//     Console.Write(Akkerman(m, n)); 
+// } 
+ 
+// int Akkerman(int m, int n) 
+// { 
+//     if (m == 0) 
+//     { 
+//         return n + 1; 
+//     } 
+//     else if (n == 0 && m > 0) 
+//     { 
+//         return Akkerman(m - 1, 1); 
+//     } 
+//     else 
+//     { 
+//         return (Akkerman(m - 1, Akkerman(m, n - 1))); 
+//     } 
 // }
-//     if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
-// {
-// Console.WriteLine("Такого элемента нет  ");
-// }
-//     else
-// {
-//     Console.WriteLine("Значение = {0}", arr[pos1, pos2]);
-// }
-// Console.ReadLine();
 
 
-/////////////////////////////////////////////////////////
+// =================================================================================================
 
-
-
-//   Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
-
-
-
-// using System;
-
-// class Program
-// {
-//     static void Main()
-//     {
-//         // Пример двумерного масси
-//         int[,] myArray = {
-//             {1, 2, 3},
-//             {4, 5, 6},
-//             {7, 8, 9}
-//         };
-
-//         Console.WriteLine("Mассив:");
-//         PrintArray(myArray);
-
-//         SwapFirstAndLastRow(myArray);
-
-//         Console.WriteLine("Массив:");
-//         PrintArray(myArray);
-//     }
-
-//     static void SwapFirstAndLastRow(int[,] array)
-//     {
-
-//         int rows = array.GetLength(0);
-//         if (rows >= 2)
-//         {
-//             for (int i = 0; i < array.GetLength(1); i++)
-//             {
-//                 int temp = array[0, i];
-//                 array[0, i] = array[rows - 1, i];
-//                 array[rows - 1, i] = temp;
-//             }
-//         }
-//     }
-
-//     static void PrintArray(int[,] array)
-//     {
-//         for (int i = 0; i < array.GetLength(0); i++)
-//         {
-//             for (int j = 0; j < array.GetLength(1); j++)
-//             {
-//                 Console.Write(array[i, j] + " ");
-//             }
-//             Console.WriteLine();
-//         }
-//      }
-//             }
-
-// =================================================================================
-
-/* Задайте прямоугольный двумерный массив. 
-Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-*/
-
-
-// using System;
-
-// class Program
-// {
-//     static void Main()
-//     {
-    
-//         int[,] myArray = {
-//             {1, 2, 3},
-//             {4, 5, 6},
-//             {7, 8, 9},
-//             {10, 11, 12}
-//         };
-
-    
-//         Console.WriteLine("Исходный массив:");
-//         PrintArray(myArray);
-
-    
-//         int minRow = FindRowWithMinSum(myArray);
-
-    
-//         Console.WriteLine($"\nСтрока с наименьшей суммой элементов: {minRow + 1}");
-//     }
-
-//     static int FindRowWithMinSum(int[,] array)
-//     {
-//         int minSum = int.MaxValue;
-//         int minRow = -1;
-
-//         for (int i = 0; i < array.GetLength(0); i++)
-//         {
-//             int currentSum = 0;
-//             for (int j = 0; j < array.GetLength(1); j++)
-//             {
-//                 currentSum += array[i, j];
-//             }
-
-//             if (currentSum < minSum)
-//             {
-//                 minSum = currentSum;
-//                 minRow = i;
-//             }
-//         }
-
-//         return minRow;
-//     }
-
-//     static void PrintArray(int[,] array)
-//     {
-//         for (int i = 0; i < array.GetLength(0); i++)
-//         {
-//             for (int j = 0; j < array.GetLength(1); j++)
-//             {
-//                 Console.Write(array[i, j] + " ");
-//             }
-//             Console.WriteLine();
-//         }
-//     }
+// Задача 3: Задайте произвольный массив.  
+// Выведете его элементы, начиная с конца.  
+// Использовать рекурсию, не использовать циклы. 
+// class MainReturn 
+// { 
+// static void Main(string[] args) 
+//     { 
+//         int[] array = { 1, 2, 5, 0, 10, 34 }; 
+ 
+//         PrintArrayReversed(array, array.Length - 1); 
+//     } 
+ 
+//     static void PrintArrayReversed(int[] arr, int index) 
+//     { 
+//         if (index >= 0) 
+//         { 
+//             Console.Write(arr[index] + " " ); 
+//             PrintArrayReversed(arr, index - 1); 
+//         } 
+  
+//     } 
 // }
